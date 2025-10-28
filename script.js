3c0199f348e8e2a87564f9f5883108baced7669b
Soumi Pal <soumiisc2020@gmail.com>
2025-10-28 17:02:59 +0530

    JS practice

---
+++ script.js
@@ -0,0 +1,58 @@
+// alert("Hello World!");
+
+console.log("Hello World!");
+
+var a = "Soumi";
+let b = "Pal";
+const c = "NSHM";
+
+let a = "hey";
+a = "Updated";
+console.log(a);
+
+var a = "hey";
+a = "Updated";
+console.log(a);
+
+const a = 'hey';
+a = 'updated';
+console.log(a);
+
+{
+    let a = "hello";
+}
+console.log(a);
+
+console.log("Name : " + "Soumi");
+console.info("Age : " + 21);
+console.warn("City : " + "Duragpur");
+
+// let name = prompt("What is your name?");
+// alert("Welcome, " + name + "!");
+
+let a = 12;
+console.log(typeof (a));
+
+// let age = prompt("Enter age:"); console.log(age + 5);
+
+let a = 12
+console.log(a)
+
+console.log("Hello World!")
+console.log("Hey there!")
+console.log("What's up!")
+
+console.log(undefined+1);
+
+let abj1 = { 
+                name: "Soumi" 
+            }; 
+let obj2 = obj1; 
+obj2.name = "Pal"; 
+console.log(obj1.name);
+
+let msg = "I love Github";
+console.log(msg.slice(2, 6));
+console.log(msg.split(""));
+console.log(msg.replace("love", "work at"));
+console.log(msg.includes("love"));   
